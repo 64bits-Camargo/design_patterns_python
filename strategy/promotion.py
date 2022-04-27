207620b2a11229844c1e642417f813da12152019
Mateus Camargo <mateusca@bancomoneo.com>
2022-04-27 14:02:08 -0300

    Clean Code - remove magics number

--- strategy/promotion.py
+++ strategy/promotion.py
@@ -1,23 +1,32 @@
+FIDELITY_PERCENT_DISCOUNT = 0.05
+BULK_ITEM_PERCENT_DISCOUNT = 0.1
+LARGE_ORDER_PERCENT_DISCOUNT = 0.07
+
+MIN_FIDELITY_POINTS_DISCOUNT = 1000
+MIN_PRODUCTS_LARGE_DISCOUNT = 10
+MIN_BULK_ITEM_DISCOUNT = 20
+
+
 def fidelity_promo(order):
     discount = 0
-    if order.customer.fidelity_points >= 1000:
-        discount = order.subtotal * 0.05
+    if order.customer.fidelity_points >= MIN_FIDELITY_POINTS_DISCOUNT:
+        discount = order.subtotal * FIDELITY_PERCENT_DISCOUNT
     return discount
 
 
 def bulk_item_promo(order):
     discount = 0
     for product in order.products:
-        if product.quantity >= 20:
-            discount += product.total * 0.1
+        if product.quantity >= MIN_BULK_ITEM_DISCOUNT:
+            discount += product.total * BULK_ITEM_PERCENT_DISCOUNT
     return discount
 
 
 def large_order_promo(order):
     discount = 0
     distinct_products = list(set(order.products))
-    if len(distinct_products) >= 10:
-        discount = order.subtotal * 0.07
+    if len(distinct_products) >= MIN_PRODUCTS_LARGE_DISCOUNT:
+        discount = order.subtotal * LARGE_ORDER_PERCENT_DISCOUNT
     return discount
 
 
