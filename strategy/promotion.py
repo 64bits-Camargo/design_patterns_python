d58fce993468f81311140d63b9fb626199e09c0f
Mateus Camargo <mateusca@bancomoneo.com>
2022-04-27 13:27:10 -0300

    Updated - Design strategy get function scope global

--- strategy/promotion.py
+++ strategy/promotion.py
@@ -21,6 +21,9 @@ def large_order_promo(order):
     return discount
 
 
+promos = [globals()[name] for name in globals()
+          if name.endswith('_promo') and name != 'best_promo']
+
+
 def best_promo(order):
-    promos = [fidelity_promo, bulk_item_promo, large_order_promo]
     return max(promo(order) for promo in promos)
