5b6d6620c23b484ede1d5b0c9ef093a8f02286f8
Mateus Camargo <mateusca@bancomoneo.com>
2022-05-02 14:03:34 -0300

    Design Pattern - Strategy - Decorator for create list promos.

--- strategy/promotion.py
+++ strategy/promotion.py
@@ -6,7 +6,15 @@ MIN_FIDELITY_POINTS_DISCOUNT = 1000
 MIN_PRODUCTS_LARGE_DISCOUNT = 10
 MIN_BULK_ITEM_DISCOUNT = 20
 
+promos = []
 
+
+def promotion(promo_func):
+    promos.append(promo_func)
+    return promos
+
+
+@promotion
 def fidelity_promo(order):
     discount = 0
     if order.customer.fidelity_points >= MIN_FIDELITY_POINTS_DISCOUNT:
@@ -14,6 +22,7 @@ def fidelity_promo(order):
     return discount
 
 
+@promotion
 def bulk_item_promo(order):
     discount = 0
     for product in order.products:
@@ -22,16 +31,13 @@ def bulk_item_promo(order):
     return discount
 
 
+@promotion
 def large_order_promo(order):
     discount = 0
     distinct_products = list(set(order.products))
     if len(distinct_products) >= MIN_PRODUCTS_LARGE_DISCOUNT:
         discount = order.subtotal * LARGE_ORDER_PERCENT_DISCOUNT
     return discount
-
-
-promos = [globals()[name] for name in globals()
-          if name.endswith('_promo') and name != 'best_promo']
 
 
 def best_promo(order):
